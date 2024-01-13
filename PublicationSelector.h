d6647713acf60267c5b791ff104c667889c17852
Agam Airi <75530266+agamairi@users.noreply.github.com>
2024-01-13 00:07:54 -0500

    Update PublicationSelector.h

--- PublicationSelector.h
+++ PublicationSelector.h
@@ -1,12 +1,3 @@
-// Final Project 
-// PublicationSelector
-// File PublicationSelector.h
-// Version 1.0
-// Author	Fardad Soleimanloo
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-/////////////////////////////////////////////////////////////////
 #ifndef SDDS_PUBLICATIONSELECTOR_H
 #define SDDS_PUBLICATIONSELECTOR_H
 #include "Publication.h"
